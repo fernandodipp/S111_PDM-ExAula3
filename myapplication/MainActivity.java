bcc080613ad399e4daeaf58e7c836a2c9cdb2839
Fernando Luigi <61151796+fernandodipp@users.noreply.github.com>
2021-08-08 16:53:50 -0300

    Add files via upload

---
+++ myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+/*
+Criar um projeto Android, para que rode com a API Level mínimo 25.
+A primeira Activity deve ser uma tela de login, com as entradas para digitar usuário e senha.
+Ao clicar no botão de logar, verificar se user está cadastrado no sistema (user=admin,
+senha=123).
+Mostrar uma mensagem informando que usuário e senha é inválido. Caso esteja ok,
+enviar o usuário para a próxima Activity, onde deve ser mostrado as informações digitadas.
+ */
+
+
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abreSegundaActivity(View view) {
+
+        EditText campoLogin = findViewById(R.id.editText_Login);
+        EditText campoSenha = findViewById(R.id.editText_Password);
+        String loginDigitado = campoLogin.getText().toString();
+        String senhaDigitada = campoSenha.getText().toString();
+
+        if(loginDigitado.equals("admin") && senhaDigitada.equals("123")){
+            Bundle sacolaParametros = new Bundle();
+            sacolaParametros.putString("TEXTO_LOGIN",loginDigitado);
+            sacolaParametros.putString("TEXTO_SENHA",senhaDigitada);
+            Intent intent = new Intent(this,MainActivity2.class);
+            intent.putExtras(sacolaParametros);
+            startActivity(intent);
+        }else{
+            Toast toast = Toast.makeText(this, "Login ou Senha incorreta!" , Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+
+
+
+
+    }
+}
---
+++ myapplication/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView campoLogin= findViewById(R.id.recebe_usuario);
+        TextView campoSenha= findViewById(R.id.recebe_senha);
+
+        // aqui estou pegando o parametro enviado pelo meu pai
+        campoLogin.setText((String) getIntent().getSerializableExtra("TEXTO_LOGIN"));
+        campoSenha.setText((String) getIntent().getSerializableExtra("TEXTO_SENHA"));
+
+    }
+
+
+}
